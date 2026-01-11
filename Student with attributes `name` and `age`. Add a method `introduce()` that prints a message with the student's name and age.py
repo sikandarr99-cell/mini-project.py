f5349dde3436d10607a0c99af1a2a59d1d05d7aa
sikandarr99-cell <sikandarr99@gmail.com>
2026-01-12 01:53:32 +0500

    Add files via upload

---
+++ Student with attributes `name` and `age`. Add a method `introduce()` that prints a message with the student's name and age.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"Hi, my name is {self.name} and I am {self.age} years old.")
+
+
+student = Student("Sikandar", 25)
+student.introduce()
